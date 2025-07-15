202204b5bc26de5fda952f36033cc96e5fedd353
IsaiMendezGarcia <isaindie2804@hotmail.com>
2025-07-15 11:17:18 -0600

    cambio de variables

--- start.bat
+++ start.bat
@@ -1,11 +1,11 @@
 echo @echo off
-set "CLONE_DIR=%USERPROFILE%\Documents\UAT"
+set "START_DIR=%USERPROFILE%\Documents\UAT"
 echo title Servidor de Desarrollo UAT
 echo echo Iniciando servidor UAT... Por favor, no cierres esta ventana.
 echo =================================
 echo        Dinamometro UAT
 echo    Creado por INSOEL and UNAM
 echo =================================
-echo cd /d "%CLONE_DIR%"
+echo cd /d "%START_DIR%"
 npm start
 start chrome http://localhost:3000/
